868836c05ee9cb975d001398662b2a112ab78766
Sanchez <dannysanchezgt@hotmail.com>
2024-10-13 20:14:32 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Sanchez's Blogs 2024 UNIR`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
